d5e66684dc8a24c87ae075869fa5d427780ecff0
ruuuu <rufinka_91@mail.ru>
2023-05-04 14:56:05 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './index.html';
 import './index.scss';
 // 
+
+const videoBg = document.querySelector('.video-bg');
+videoBg.innerHTML = `
+      <source srcset="video/video.webm" type="video/webm">
+      <source srcset="video/video.mp4" type="video/mp4"> 
+`;
