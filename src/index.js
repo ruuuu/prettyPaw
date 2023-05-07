0c4971b3fbe8eb7d9b210f0fa60970651db5aad7
ruuuu <rufinka_91@mail.ru>
2023-05-07 19:42:10 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
 import './index.html';
-import 'swiper/scss';  //  подключам свайпер
+import 'swiper/scss';  //  подключам свайпер сладйер
 import 'swiper/scss/pagination';
-
-
 import './index.scss';
+import { slidersInit } from './modules/sliders';
+
+
+
+slidersInit('.about__slider', {  // сладер для "О компании"
+            pagination: {
+                  el: '.about__slider-pagination',
+            }
+      }
+);
+
+
+
+slidersInit('.career__slider ', {  // сладер для "Карьера"
+      pagination: {
+            el: '.career__slider-pagination',
+      },
+      breakpoints: {
+            
+            768: {
+              slidesPerView: 3,  // кол-во слайдв на странице
+              spaceBetween: 20
+            },
+
+           
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 26
+            },
+
+            
+            1240: {
+              slidesPerView: 4,
+              spaceBetween: 30
+            }
+          }
+      }
+);
+
 
 
 const videoBg = document.querySelector('.video-bg');
---
+++ src/modules/sliders.js
@@ -0,0 +1,29 @@
+import  {Swiper, Pagination, Autoplay} from "swiper";
+
+const params = {
+
+      spaceBetween: 20,  // расстояние между слайдами
+      loop: true,  // зацикленный слайдер
+      autoplay: {
+            delay: 3500,  //  зажержка между слайдами в сек
+            disableOnInteraction: false,  // после взаимодействия со сладйером, он продолжит рабоать
+      },
+      sliderPerView: 1, // число отображаемыз слайдов на странице
+      pagination: {
+            el: '.swiper-pagination'
+      },
+      modules: [  Autoplay, Pagination ]
+};
+
+
+
+
+export const slidersInit = (selectorSlider, newParams) => {
+
+      new Swiper(selectorSlider,  {
+            ...params,
+            ...newParams,
+      }
+      );
+
+};
